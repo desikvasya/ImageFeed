d14bea5af3e5ef018296fba9d836f46a7efa914f
desikvasya <desikvasya1@gmail.com>
2022-12-28 21:32:00 +1000

    no message

---
+++ Image Feed/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  Image Feed
+//
+//  Created by Denis on 25.12.2022.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
