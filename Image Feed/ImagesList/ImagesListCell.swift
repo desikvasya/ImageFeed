be83af99f05e6a203e24e864bdc4840e1aa3ef22
desikvasya <desikvasya1@gmail.com>
2023-01-02 15:22:21 +1000

    Спринт 8 - код ревью

--- Image Feed/ImagesList/ImagesListCell.swift
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -2,4 +2,7 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var tableImage: UIImageView!
+    @IBOutlet weak var tableLike: UIButton!
+    @IBOutlet weak var tableDate: UILabel!
 }
--- Image Feed/ImagesList/ImagesListViewController.swift
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -2,6 +2,14 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0...20).map{"\($0)"}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
 
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -13,7 +21,7 @@ class ImagesListViewController: UIViewController {
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -22,6 +30,7 @@ extension ImagesListViewController: UITableViewDataSource {
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
+        imageListCell.selectionStyle = .gray
 
         configCell(for: imageListCell, with: indexPath)
 
@@ -30,9 +39,33 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { }
-}
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image: UIImage = UIImage(named: String(indexPath.row)) else {
+            return}
+        cell.tableImage.image = image
+        cell.tableDate.text = dateFormatter.string(from: Date())
+        switch (indexPath.row%2) {
+        case 0 : cell.tableLike.imageView?.image = UIImage(named: "like_button_on")
+        case 1 : cell.tableLike.imageView?.image = UIImage(named: "like_button_off")
+        default:
+            cell.tableLike.imageView?.image = UIImage(named: "like_button_off")
+        }
+        }
+    }
+
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image: UIImage = UIImage(named: String(indexPath.row)) else {
+            return 0}
+        let imageInSets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInSets.left - imageInSets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInSets.top + imageInSets.bottom
+        return cellHeight
+    }
 }
