9492243e8fb46750e94ddaab03fc0f8e5956e6eb
desikvasya <desikvasya1@gmail.com>
2023-02-10 22:21:53 +1000

    исправил ошибку - the request scope is invalid

--- Image Feed/Constants.swift
+++ Image Feed/Constants.swift
@@ -10,7 +10,7 @@ struct Constants {
     static let accessKey = "9JfQB7nGwFMCy2VbNfejIewCXKgnopIY8F_xLejNvuY"
     static let secretKey = "AHFDOFodhpa1WQZ-mixA4Z4apDu29lN8Ew9_fze7hdc"
     static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_users+likes"
+    static let accessScope = "public+read_user+write_likes"
     static let defaultBaseUrl = URL(string: "https://api.unsplash.com")!
     static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
